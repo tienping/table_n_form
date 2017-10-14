0cee16ccc6aee3b0b51beed7bfcb75c3ba45b6b2
TienPing <petwesley@gmail.com>
2017-10-15 06:29:38 +0800

    bypass http

--- assets/config.js
+++ assets/config.js
@@ -3,3 +3,5 @@ var DEBUG_MODE = false;
 var TOKEN_KEY = 'itech-auth';
 
 var API_URL = 'http://chama666.com/itec/public/api/1.0';
+// to by pass for github testing:
+API_URL = 'https://cors-anywhere.herokuapp.com/http://chama666.com/itec/public/api/1.0';
