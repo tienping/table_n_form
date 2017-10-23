36bf23cb5bea2fd7a886eb5cc88e0a3b018a2d78
TienPing <petwesley@gmail.com>
2017-10-23 08:37:32 +0800

    finish up pages

--- assets/app/itech.controller.js
+++ assets/app/itech.controller.js
@@ -3,18 +3,19 @@
 
     angular
         .module('itech.controller',[])
-        .controller('itechCtrl', itechCtrl);
+        .controller('itechCtrl', itechCtrl)
+        .filter('toHourMinute', toHourMinute);
 
     itechCtrl.$inject = ['$scope', '$rootScope', '$state', 'userRs', 'vehicleRs', 'reportRs', 'Excel', '$timeout'];
     function itechCtrl($scope, $rootScope, $state, userRs, vehicleRs, reportRs, Excel, $timeout) {
         var itech = this;
         
         itech.init           = init;
+        itech.getUserList    = getUserList;
         itech.openModal      = openModal;
         itech.toggleLanguage = toggleLanguage;
         itech.loginSubmit    = loginSubmit;
         itech.logoutSubmit   = logoutSubmit;
-        itech.initHome       = initHome;
         itech.changeTab      = changeTab;
         itech.exportToExcel  = exportToExcel;
 
@@ -28,8 +29,8 @@
 
         itech.homeSelections  = [
             {
-                label: 'Daily & Breakdown',
-                key: 'daily_and_breakdown',
+                label: 'Action',
+                key: 'action',
                 type: 'list'
             }, {
                 label: 'Report',
@@ -37,29 +38,31 @@
                 type: 'report'
             }
         ];
-        itech.tabSelected     = itech.homeSelections[0];
-        
+        itech.tabSelected = itech.homeSelections[0];
 
-        // itech.data      = {
-        //     en: {},
-        //     bm: {}
-        // }
-        // loadJSON('assets/fixture-en.json', function(response) {
-        //     itech.data.en = JSON.parse(response);
-        // });
-        // loadJSON('assets/fixture-bm.json', function(response) {
-        //     itech.data.bm = JSON.parse(response);
-        // });
-        // itech.lang      = itech.lang || 'en';
-
-        $scope.slider = {
-            value: 500,
-            options: {
-                floor: 500,
-                ceil: 3000,
-                step: 500
+        itech.companies = [
+            {
+                label: 'AA RAMP'
+            }, {
+                label: 'AA ENGINEERING'
+            }, {
+                label: 'AAX ENGINEERING'
+            }, {
+                label: 'AAX RAMP'
+            }, {
+                label: 'ACWER'
+            }, {
+                label: 'AirAsia Charter'
+            }, {
+                label: 'MACKT_M'
+            }, {
+                label: 'PCA'
+            }, {
+                label: 'MALINDO'
+            }, {
+                label: 'VISION V.R.'
             }
-        };
+        ]
 
         $rootScope.$on('$stateChangeSuccess', function() {
             if (!itech.token && $state.current.name !== 'login') {
@@ -76,6 +79,30 @@
         function init() {
             var offsetValue = 50;
             initializeSmoothScroll(offsetValue);
+
+            if (!itech.data.vehicle) {
+                // vehicleRs.save({
+                funCall({
+                    param: 'list'
+                }, function successCallback(response) {
+                    itech.data.vehicle = response.vehicles;
+                }, function failureCallback(response) {
+                    
+                }, 'vehicleRs.save');
+            }
+        }
+
+        function getUserList() {
+            if (!itech.data.userlist) {
+                // vehicleRs.save({
+                funCall({
+                    param: 'list'
+                }, function successCallback(response) {
+                    itech.data.userlist = response.users;
+                }, function failureCallback(response) {
+                    
+                }, 'userListRs.save');
+            }
         }
 
         function openModal(id) {
@@ -88,7 +115,9 @@
         }
 
         function loginSubmit() {
-            userRs.save({
+            funCall({
+
+            // userRs.save({
                 param: 'login',
                 username: 'admin',
                 password: 'password'
@@ -99,7 +128,7 @@
                 window.location.href = '/';
             }, function failureCallback(response) {
                 alert('Login Failed... Please consult system administrator.', response);
-            });
+            }, 'userRs.save');
         }
 
         function logoutSubmit() {
@@ -109,20 +138,20 @@
             location.reload();
         }
 
-        function initHome() {
-            if (!itech.data.vehicle) {
-                vehicleRs.save({
-                    param: 'list'
-                }, function successCallback(response) {
-                    itech.data.vehicle = response.vehicles;
-                }, function failureCallback(response) {
-                    
+        function changeTab(key) {
+            if (key == 'report') {
+                var FromEndDate = new Date();
+                
+                $('#itech-datepicker').datepicker({
+                    autoclose: true,
+                    minViewMode: 1,
+                    startDate: '06/2017',
+                    format: 'mm/yyyy'
+                }).on('changeDate', function(selected){
+                    FromEndDate = new Date(selected.date.valueOf());
+                    FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
                 });
             }
-        }
-
-        function changeTab(key) {
-            
         }
 
         function exportToExcel(tableQuery, fileName) {
@@ -136,4 +165,74 @@
             
         }
     }
+
+    function toHourMinute() {
+        return function(input){
+            var hours = parseInt(input / 60);
+            var minutes = input % 60;
+            
+            if (hours) {
+                return hours+' hr' + ' ' + minutes + ' min';
+            } else {
+                return minutes + ' min';
+            }
+        }
+    }
 })();
+
+// a fuction to mock up resource calling
+function funCall(params, successCallback, failureCallback, resourceKey) {
+    console.log('Function call: ', { params: params, mockResponse: mockResponse });
+
+    var mockResponse = {
+        data: 'smth'
+    }
+
+    if (resourceKey == 'userRs.save') {
+        mockResponse = {
+            token: 'mock_token',
+            user: {
+                userdata: 'userdata'
+            }
+        }
+    } else if(resourceKey == 'vehicleRs.save') {
+        mockResponse = {
+            vehicles: [
+                {
+                    name: 'vehicle 1',
+                    company_id: 4,
+                    last_check: {
+                        distance: 11,
+                        time: '119' // 1:59
+                    },
+                    num_of_uncheck: 1,
+                    num_of_breakdown: 0,
+                    last_breakdown: null
+                },
+                {
+                    name: 'vehicle 2',
+                    company_id: 1,
+                    last_check: {
+                        distance: 7,
+                        time: '39' // 0:39
+                    },
+                    num_of_uncheck: 2,
+                    num_of_breakdown: 3,
+                    last_breakdown: "2017-09-29 12:10:17"
+                }
+            ]
+        }
+    } else if(resourceKey == 'userListRs.save') {
+        mockResponse = {
+            users: [
+                {
+                    name: 'user 1'
+                },
+                {
+                    name: 'user 2'
+                }
+            ]
+        }
+    }
+    successCallback(mockResponse);
+}
