2b2c1f3bad755240665a989f06d1d59dfa28c2dd
TienPing <petwesley@gmail.com>
2017-12-01 04:43:31 +0800

    use back chama api for now

--- assets/config-local.js
+++ assets/config-local.js
@@ -1,4 +1,4 @@
-DEBUG_MODE = false;
+DEBUG_MODE = true;
 
-API_URL = 'http://itechnm.com/api/1.0';
-// API_URL = 'http://chama666.com/itec/public/api/1.0';
+// API_URL = 'http://itechnm.com/api/1.0';
+API_URL = 'http://chama666.com/itec/public/api/1.0';
