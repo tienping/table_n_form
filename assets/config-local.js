467c431d59d7cf8589eb20b8872892290fdef715
TienPing <petwesley@gmail.com>
2017-12-06 23:15:13 +0800

    update config-local use cdn font file

--- assets/config-local.js
+++ assets/config-local.js
@@ -1,5 +1,5 @@
 DEBUG_MODE = false;
-// API_URL = 'http://itechenm.com/api/1.0';
-API_URL = 'https://cors-anywhere.herokuapp.com/http://itechenm.com/api/1.0';
+API_URL = 'http://itechenm.com/api/1.0';
 
+// API_URL = 'https://cors-anywhere.herokuapp.com/http://itechenm.com/api/1.0';
 // DEBUG_MODE = true;
